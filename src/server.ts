ba4fe67a61833c5735866d2a6acf026a493df89e
Pavel Karenda <korendos@gmail.com>
2021-06-20 20:53:57 +0300

    feat: add orm to the server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,31 @@
-import * as app from './app';
+import { createConnection, getConnection } from 'typeorm';
+
+import 'reflect-metadata';
+
+import { typeOrmConfig } from './common/ormconfig';
+
+import { app } from './app';
 
 import { PORT } from './common/config';
 
-app.app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+(async () => {
+  let connection;
+  try {
+    connection = getConnection()
+  } catch(err) {
+    console.error(err);
+  }
+  try{
+    if (connection) {
+      if(connection.isConnected) await connection.connect()
+    } else {
+      await createConnection(typeOrmConfig);
+    }
+    console.log('Connected!');
+
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+})();
