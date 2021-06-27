765997ae2acf17e7b542a5002ec9804998151e79
Pavel Karenda <korendos@gmail.com>
2021-06-27 20:53:31 +0300

    feat: add secret key

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,7 +5,7 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
-export const { PORT, POSTGRES_PORT, POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB, POSTGRES_HOST } = process.env;
+export const { PORT, POSTGRES_PORT, POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB, POSTGRES_HOST, JWT_SECRET_KEY } = process.env;
 export const { NODE_ENV } = process.env;
 const { AUTH_MODE: authMode } = process.env;
 export const AUTH_MODE = authMode === 'true';
